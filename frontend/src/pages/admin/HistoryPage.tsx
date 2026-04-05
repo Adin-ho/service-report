a8f142a74eba232ccdce0fa0429acb863c9fd020
Adin-ho <adinnugroho854@gmail.com>
2026-04-05 20:13:24 +0700

    test

--- frontend/src/pages/admin/HistoryPage.tsx
+++ frontend/src/pages/admin/HistoryPage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { QrCode } from "lucide-react";
 import { api } from "../../lib/api";
-
+//
 interface ReportHistory {
   id: number;
   dispatch_no: string;
